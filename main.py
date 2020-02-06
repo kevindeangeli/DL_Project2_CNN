66cdd8657ca999172d89ddc199475fadcee06f97
Kevin De Angeli <kevindeangeli94@gmail.com>
2020-02-06 12:28:16 -0500

    Setting up project

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Created by: Kevin De Angeli
+Email: kevindeangeli@utk.edu
+Date: 2020-02-06
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sym
+
+
+def main():
+    print("First program")
+
+if __name__ == "__main__":
+    main()
